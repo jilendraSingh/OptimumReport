f1248f5008c40c10be56954e3d29c9c78883813b
Jilendar <jilendra.singh@ajath.com>
2022-10-13 17:22:49 +0530

    Option menu with Logout functionality 13 OCT 2022

--- app/src/main/java/com/optimum/optimumreport/MainActivity.kt
+++ app/src/main/java/com/optimum/optimumreport/MainActivity.kt
@@ -1,19 +1,19 @@
 package com.optimum.optimumreport
 
+
+import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.optimum.optimumreport.adapter.OutLetAdapter
 import com.optimum.optimumreport.adapter.ReportListAdapter
 import com.optimum.optimumreport.databinding.ActivityMainBinding
-import com.optimum.optimumreport.databinding.FragmentOutletLocationBinding
 import com.optimum.optimumreport.interfaces.OnItemClickListener
 import com.optimum.optimumreport.model.OutletModel
 import com.optimum.optimumreport.model.ReportModel
@@ -21,6 +21,7 @@ import com.optimum.optimumreport.utility.Constants
 import com.optimum.optimumreport.utility.NetworkResult
 import com.optimum.optimumreport.utility.Utility
 import com.optimum.optimumreport.viewmodel.CafePosViewModel
+
 
 class MainActivity : AppCompatActivity() , OnItemClickListener {
 
@@ -31,19 +32,35 @@ class MainActivity : AppCompatActivity() , OnItemClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.include2.tvTitle.text="Outlet Location"
+        sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE)
+        val toolbarTitle = binding.include2.toolbar.findViewById(R.id.tv_title) as TextView
+        toolbarTitle.text = "Outlet Location"
+        binding.include2.toolbar.inflateMenu(R.menu.optionmenu)
+        binding.include2.toolbar.overflowIcon?.setTint(resources.getColor(R.color.white))
 
         viewModel = ViewModelProvider(this@MainActivity)[CafePosViewModel::class.java]
         sharedPreferences = getSharedPreferences("MySharedPref", AppCompatActivity.MODE_PRIVATE)
 
         getOutLetLocationList()
+        binding.include2.toolbar.setOnMenuItemClickListener { item ->
+            if (item.itemId == R.id.logout) {
+                val editor = sharedPreferences.edit()
+                editor.clear()
+                editor.apply()
+                val intent = Intent(this@MainActivity, LoginActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(intent)
+                finish()
+            }
+            false
+        }
     }
 
     private fun getOutLetLocationList() {
-          val userCode = sharedPreferences.getInt(Constants.USER_CODE, 0)
+        val userCode = sharedPreferences.getInt(Constants.USER_CODE, 0)
         viewModel.getOutletLocationList(userCode).observe(this@MainActivity) { response ->
             when (response) {
                 is NetworkResult.Success -> {
@@ -77,26 +94,27 @@ class MainActivity : AppCompatActivity() , OnItemClickListener {
     }
 
     private fun setUpData(data: OutletModel?) {
-        try{
+        try {
 
-                binding.rvOutletList.layoutManager =
-                    LinearLayoutManager(this@MainActivity, RecyclerView.HORIZONTAL, false)
-                binding.rvOutletList.isNestedScrollingEnabled = true
-                binding.rvOutletList.adapter = OutLetAdapter(data?.data!!.locations, this)
+            binding.rvOutletList.layoutManager =
+                LinearLayoutManager(this@MainActivity, RecyclerView.HORIZONTAL, false)
+            binding.rvOutletList.isNestedScrollingEnabled = true
+            binding.rvOutletList.adapter = OutLetAdapter(data?.data!!.locations, this)
 
-        }catch (e:Exception){
-            Toast.makeText(this@MainActivity,e.printStackTrace().toString(), Toast.LENGTH_SHORT).show()
+        } catch (e: Exception) {
+            Toast.makeText(this@MainActivity, e.printStackTrace().toString(), Toast.LENGTH_SHORT)
+                .show()
         }
 
     }
 
     private fun setUpReportData(data: ReportModel?) {
 
-            binding.rvReportTypeList.layoutManager =
-                LinearLayoutManager(this@MainActivity)
-            binding.rvReportTypeList.isNestedScrollingEnabled = true
-            binding.rvReportTypeList.adapter =
-                ReportListAdapter(this@MainActivity, data!!.data, locationCodeData)
+        binding.rvReportTypeList.layoutManager =
+            LinearLayoutManager(this@MainActivity)
+        binding.rvReportTypeList.isNestedScrollingEnabled = true
+        binding.rvReportTypeList.adapter =
+            ReportListAdapter(this@MainActivity, data!!.data, locationCodeData)
 
     }
 
